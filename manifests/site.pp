b45e41ae44d8ffd8142dcc581b1c8304779bb68a
Léonard TAVAE <leonard.tavae@informatique.gov.pf>
2013-07-10 22:23:23 -1000

    Ajout de la variable php, permettant d'activer le module php.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,39 +1,61 @@
 # Definition
 #
 
-define apache::site($vhosts,$vapps,$status="desactivate") {
+define apache::site($vhosts,$vapps,$status="desactivate",$php="disabled") {
+
 	include apache
+
 	file { "/etc/apache2/sites-available/${vhosts}":
 		path    => "/etc/apache2/sites-available/${vhosts}",
 		ensure  => file,
-		require => package['apache2'],
+		require => Package['apache2'],
 		content => template("apache/apache-vhost.erb"),
-		notify  => service['apache2']
+		notify  => Service['apache2']
 	}
 
 	file { "/var/www/${vapps}":
 		path    => "/var/www/${vapps}",
-		ensure  => directory
+		ensure  => directory,
+		owner	=> www-data,
+		group	=> www-data
 	}
 
 	case $status {
 		'activate' : {
 			exec { "/usr/sbin/a2ensite ${vhosts}":
 				unless	=> "/bin/readlink -e /etc/apache2/sites-enabled/${vhosts}",
-				notify	=> exec["reload-apache2"],
-				require	=> package["apache2"],
+				notify	=> Exec["reload-apache2"],
+				require	=> Package["apache2"],
 			}
 		}
 		'desactivate': {
 			exec { "/usr/sbin/a2dissite ${vhosts}":
 				onlyif	=> "/bin/readlink -e /etc/apache2/sites-enabled/${vhosts}",
-				notify	=> exec["reload-apache2"],
-				require	=> package["apache2"],
+				notify	=> Exec["reload-apache2"],
+				require	=> Package["apache2"],
 			}
 		}
 		default: {
-			err ("La valeur du parametre status est inconnu")
+			err ("La valeur du parametre status est inconnue.")
 		}
+	}
+
+	case $php {
+		'enable' : {
+		        package { 'libapache2-mod-php5':
+				ensure => installed,
+				notify	=> Exec["reload-apache2"]
+			}
+		}
+		'disabled' : {
+		        package { 'libapache2-mod-php5':
+				ensure => purged,
+				notify	=> Exec["reload-apache2"]
+			}
+		}
+		default: {
+			err ("La valeur du parametre php est inconnue.")
+		}		
 	}
 
 	exec { "reload-apache2":
