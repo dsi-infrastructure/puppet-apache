4d910c41789fcee634fd8b68b96c58ea827dc6bf
Léonard TAVAE <leonard.tavae@informatique.gov.pf>
2013-07-14 15:42:50 -1000

    Validation syntaxique.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,23 +1,23 @@
 # Class apache
 #
-class apache($status="disabled", $apachemodules) {
+class apache($apachemodules, $status='disabled') {
   package { 'apache2':
-		ensure => installed
-	}
+    ensure => installed,
+  }
 
-	package { $apachemodules:
-		ensure => installed
-	}
+  package { $apachemodules:
+    ensure => installed,
+  }
 
-	service { "apache2":
-		ensure 		=> running,
-		hasstatus 	=> true,
-		hasrestart	=> true,
-		require		=> Package["apache2"],
-	}
+  service { 'apache2':
+    ensure      => running,
+    hasstatus   => true,
+    hasrestart  => true,
+    require     => Package['apache2'],
+  }
 
-  exec { "reload-apache2":
-    command => "/etc/init.d/apache2 reload",
+  exec { 'reload-apache2':
+    command     => '/etc/init.d/apache2 reload',
     refreshonly => true,
   }
 }
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,43 +1,41 @@
 # Definition
 #
-define apache::site($vhosts,$vapps,$status="disabled",$ssl="disabled") {
+define apache::site($vhosts,$vapps,$status='disabled',$ssl='disabled') {
+  include apache
 
-	include apache
+  file { "/etc/apache2/sites-available/${vhosts}":
+    ensure  => file,
+    path    => "/etc/apache2/sites-available/${vhosts}",
+    require => Package['apache2'],
+    content => template('apache/apache-vhost.erb'),
+    notify  => Service['apache2']
+  }
 
-	file { "/etc/apache2/sites-available/${vhosts}":
-		path    => "/etc/apache2/sites-available/${vhosts}",
-		ensure  => file,
-		require => Package['apache2'],
-		content => template("apache/apache-vhost.erb"),
-		notify  => Service['apache2']
-	}
+  file { "/var/www/${vapps}":
+    ensure  => directory,
+    path    => "/var/www/${vapps}",
+    owner   => www-data,
+    group   => www-data
+  }
 
-	file { "/var/www/${vapps}":
-		path    => "/var/www/${vapps}",
-		ensure  => directory,
-		owner	=> www-data,
-		group	=> www-data
-	}
-
-	case $status {
-		'enable' : {
-			exec { "/usr/sbin/a2ensite ${vhosts}":
-				unless	=> "/bin/readlink -e /etc/apache2/sites-enabled/${vhosts}",
-				notify	=> Exec["reload-apache2"],
-				require	=> Package["apache2"],
-			}
-		}
-    
+  case $status {
+    'enable' : {
+      exec { "/usr/sbin/a2ensite ${vhosts}":
+        unless  => "/bin/readlink -e /etc/apache2/sites-enabled/${vhosts}",
+        notify  => Exec['reload-apache2'],
+        require => Package['apache2'],
+      }
+    }
     'disabled': {
-			exec { "/usr/sbin/a2dissite ${vhosts}":
-				onlyif	=> "/bin/readlink -e /etc/apache2/sites-enabled/${vhosts}",
-				notify	=> Exec["reload-apache2"],
-				require	=> Package["apache2"],
-			}
-		}
-		default: {
-			err ("La valeur du parametre status est inconnue.")
-		}
-	}
+      exec { "/usr/sbin/a2dissite ${vhosts}":
+        onlyif  => "/bin/readlink -e /etc/apache2/sites-enabled/${vhosts}",
+        notify  => Exec['reload-apache2'],
+        require => Package['apache2'],
+      }
+    }
+    default: {
+      err ('La valeur du parametre status est inconnue.')
+    }
+  }
 
 }
